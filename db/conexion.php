6c03df7d586d1af3ececa49ea940c82b04585a9c
Andres ° <edwinandresrangelgomez8@gmail.com>
2025-10-05 17:21:48 -0500

    Update conexion.php

--- db/conexion.php
+++ db/conexion.php
@@ -1,13 +1,18 @@
 <?php
 $host = "localhost";
-$usuario = "root";
-$password = "123456789";
-$base_datos = "proyecto_formativo";
+$usuario = "juan_admin";
+$password = "juanes2Soto";
+$base_datos = "juan_proyecto_formativo";
 
 $conn = new mysqli($host, $usuario, $password, $base_datos);
 
 if ($conn->connect_error) {
-    error_log("Error de conexión: " . $conn->connect_error); 
-    exit(); 
+    header('Content-Type: application/json');
+    echo json_encode([
+        "active" => false,
+        "error" => "Error de conexión a la base de datos: " . $conn->connect_error,
+        "timestamp" => time()
+    ]);
+    exit;
 }
 ?>
